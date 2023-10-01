85f5be355dfcf4157bb38f1a90e96607e6cf61d1
bobrov.da <dmr.bobrov@yandex.ru>
2023-10-01 20:54:36 +0300

    Add AbstractFactory

--- GenerativePatterns/FactoryMethod/Core/Developre.cs
+++ GenerativePatterns/FactoryMethod/Core/Developre.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace FactoryMethod.Core
 {
     // абстрактный класс строительной компании
+    //Creater
     public abstract class Developer
     {
         public string Name { get; set; }
--- GenerativePatterns/FactoryMethod/Core/House.cs
+++ GenerativePatterns/FactoryMethod/Core/House.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace FactoryMethod.Core
 {
+    //Product
     public abstract class House
     {
     }
