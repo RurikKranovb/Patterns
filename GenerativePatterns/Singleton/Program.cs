3ce32623d5dc439c7f78a2a552e9101feacb71f8
bobrov.da <dmr.bobrov@yandex.ru>
2023-10-01 22:49:28 +0300

    Add Singleton

---
+++ GenerativePatterns/Singleton/Program.cs
@@ -0,0 +1,25 @@
+﻿using Singleton.Infrastructure;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Computer comp = new Computer();
+        //comp.Launch("Windows");
+        //Console.WriteLine(comp.OS.Name);
+
+        //comp.OS = OS.getInstance("Linux");
+        //Console.WriteLine(comp.OS.Name);
+
+        (new Thread(() =>
+        {
+            Computer comp2 = new Computer();
+            comp2.OS = OS.getInstance("Linux");
+            Console.WriteLine(comp2.OS.Name);
+        })).Start();
+
+        Computer comp = new Computer();
+        comp.Launch("Windows");
+        Console.WriteLine(comp.OS.Name);        
+    }
+}
