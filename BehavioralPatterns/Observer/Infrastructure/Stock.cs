59b9f63cd283a91a41ae9aef7103993cfab779a8
bobrov.da <dmr.bobrov@yandex.ru>
2023-10-02 22:03:44 +0300

    Add Observer

---
+++ BehavioralPatterns/Observer/Infrastructure/Stock.cs
@@ -0,0 +1,46 @@
+﻿using Observer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer.Infrastructure
+{
+    internal class Stock : IObservable
+    {
+        StockInfo sInfo; // информация о торгах
+
+        List<IObserver> observers;
+        public Stock()
+        {
+            observers = new List<IObserver>();
+            sInfo = new StockInfo();
+        }
+        public void RegisterObserver(IObserver o)
+        {
+            observers.Add(o);
+        }
+
+        public void RemoveObserver(IObserver o)
+        {
+            observers.Remove(o);
+        }
+
+        public void NotifyObservers()
+        {
+            foreach (IObserver o in observers)
+            {
+                o.Update(sInfo);
+            }
+        }
+
+        public void Market()
+        {
+            Random rnd = new Random();
+            sInfo.USD = rnd.Next(20, 40);
+            sInfo.Euro = rnd.Next(30, 50);
+            NotifyObservers();
+        }
+    }
+}
---
+++ BehavioralPatterns/Observer/Program.cs
@@ -0,0 +1,66 @@
+﻿using Observer.Infrastructure;
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Stock stock = new Stock();
+        Bank bank = new Bank("ЮнитБанк", stock);
+        Broker broker = new Broker("Иван Иваныч", stock);
+        // имитация торгов
+        stock.Market();
+        // брокер прекращает наблюдать за торгами
+        broker.StopTrade();
+        // имитация торгов
+        stock.Market();
+
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+//interface IObservable
+//{
+//    void AddObserver(IObserver o);
+//    void RemoveObserver(IObserver o);
+//    void NotifyObservers();
+//}
+
+//class ConcreteObservable : IObservable
+//{
+
+//    private List<IObserver> observers;
+//    public ConcreteObservable()
+//    {
+//        observers = new List<IObserver>();
+//    }
+//    public void AddObserver(IObserver o)
+//    {
+//        observers.Add(o);
+//    }
+
+//    public void NotifyObservers()
+//    {
+//        foreach (IObserver observer in observers)
+//            observer.Update();
+//    }
+
+//    public void RemoveObserver(IObserver o)
+//    {
+//        observers.Remove(o);
+//    }
+//}
+
+//interface IObserver
+//{
+//    void Update();
+//}
+
+//class ConcreteObserver : IObserver
+//{
+//    public void Update()
+//    {
+        
+//    }
+//}
