88d13ddd39ad5ba93fff243378952a95bd61e48d
bobrov.da <dmr.bobrov@yandex.ru>
2023-10-02 18:57:21 +0300

    Add Prototype

---
+++ GenerativePatterns/Prototype/Infrastructure/Circle.cs
@@ -0,0 +1,30 @@
+﻿using Prototype.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype.Infrastructure
+{
+    internal class Circle : IFigure
+    {
+        int radius;
+
+        public Circle(int r)
+        {
+            radius = r;
+        }
+
+        public IFigure Clone()
+        {
+            return new Circle(radius);
+            //return MemberwiseClone() as IFigure;
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Круг радиусом {radius}");
+        }
+    }
+}
---
+++ GenerativePatterns/Prototype/Infrastructure/Rectangle.cs
@@ -0,0 +1,31 @@
+﻿using Prototype.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype.Infrastructure
+{
+    internal class Rectangle : IFigure
+    {
+        int width;
+        int height;
+        
+        public Rectangle(int w, int h)
+        {
+            width = w;
+            height = h;
+        }
+
+        public IFigure Clone()
+        {
+            return new Rectangle(width, height);
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Прямоугольник длиной {height} и шириной {width}");
+        }
+    }
+}
